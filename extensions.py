4c606431c30d43c0d434643e564f2e65131850d4
Самарский Алексей Сергеевич <samarskiy_a_s@sunlight.net>
2021-07-12 22:46:56 +0300

    token

--- extensions.py
+++ extensions.py
@@ -9,7 +9,8 @@ class Curr:
     
     @staticmethod
     def get_price(base, quote, amount):
-        res = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote}&tsyms={base}')
+        res = requests.get(f'https://min-api.cryptocompare.com/data/price\
+?fsym={quote}&tsyms={base}&api_key={CUR_TOKEN}')
         obj = json.loads(res.text)
         cur_value = obj[base]
         return cur_value * float(amount)
